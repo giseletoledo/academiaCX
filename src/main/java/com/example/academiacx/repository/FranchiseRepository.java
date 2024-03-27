1b6f259462e79c05cf7079556da16a1f131c7e0e
gisele <gisesonia@gmail.com>
2024-03-26 22:58:14 -0300

    refactor: repository de franchise

---
+++ src/main/java/com/example/academiacx/repository/FranchiseRepository.java
@@ -0,0 +1,8 @@
+package com.example.academiacx.repository;
+
+import com.example.academiacx.models.FranchiseModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FranchiseRepository extends JpaRepository<FranchiseModel, Long> {
+}
+
