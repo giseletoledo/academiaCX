63a9a8792016eb3d3dbac7e7ba4eddab38393f73
gisele <gisesonia@gmail.com>
2024-03-27 16:43:15 -0300

    refactor(repository): query do jpa

--- src/main/java/com/example/academiacx/repository/UserRepository.java
+++ src/main/java/com/example/academiacx/repository/UserRepository.java
@@ -2,7 +2,26 @@ package com.example.academiacx.repository;
 
 import com.example.academiacx.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<UserModel, Long> {
+
+    UserModel findByName (String name);
+
+    UserModel findByEmail (String email);
+
+    UserModel findByEmailAndName(String email, String name);
+
+    @Query("from UserModel u where u.name = :name") //JPQL
+    UserModel busquePorNome (String name);
+
+    @Query("from UserModel u where u.email = :email") //JPQL
+    UserModel busquePorEmail (@Param("email") String email);
+
+    @Query("SELECT u FROM UserModel u JOIN FETCH u.roles where u.username = :username")
+    UserModel findByUsernameFetchRoles(@Param("username") String username);
+
+    UserModel findByUsername(String username);
 
 }
