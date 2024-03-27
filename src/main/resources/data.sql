2b9de4cbaf233dd56cfcc5d3f71d5a5554f100dc
gisele <gisesonia@gmail.com>
2024-03-26 21:15:36 -0300

    feat(sql): adicionando dados no h2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_user(name, email) VALUES ('Maria', 'maria@gmail.com');
+INSERT INTO tb_user(name, email) VALUES ('Bob', 'bob@gmail.com');
+INSERT INTO tb_user(name, email) VALUES ('Alex', 'alex@gmail.com');
+INSERT INTO tb_user(name, email) VALUES ('Ana', 'ana@gmail.com');
+INSERT INTO tb_user(name, email) VALUES ('Joao', 'joao@gmail.com');
+INSERT INTO tb_user(name, email) VALUES ('Marcello', 'marcello@gmail.com');
+
+
+INSERT INTO tb_movie(title, genre) VALUES ('Batman Origins', 'Action');
+INSERT INTO tb_movie(title, genre) VALUES ('Spider-Verse', 'joao@gmail.com');
+INSERT INTO tb_movie(title, genre) VALUES ('Marcello', 'marcello@gmail.com');
