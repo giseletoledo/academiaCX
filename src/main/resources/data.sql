f4b3e46f2360f9e8b8a754b91dab55e94bc1aed0
gisele <gisesonia@gmail.com>
2024-03-27 17:53:08 -0300

    feat(sql): removendo tabela de movies

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,7 +5,3 @@ INSERT INTO tb_user(name, email) VALUES ('Ana', 'ana@gmail.com');
 INSERT INTO tb_user(name, email) VALUES ('Joao', 'joao@gmail.com');
 INSERT INTO tb_user(name, email) VALUES ('Marcello', 'marcello@gmail.com');
 
-
-INSERT INTO tb_movie(title, genre) VALUES ('Batman Origins', 'Action');
-INSERT INTO tb_movie(title, genre) VALUES ('Spider-Verse', 'joao@gmail.com');
-INSERT INTO tb_movie(title, genre) VALUES ('Marcello', 'marcello@gmail.com');
