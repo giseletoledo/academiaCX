fd6f82f6b5444db47e962716138c035f024d7662
gisele <gisesonia@gmail.com>
2024-03-31 01:22:28 -0300

    refactor(model): removendo comentários

--- src/main/java/com/example/academiacx/models/StudioModel.java
+++ src/main/java/com/example/academiacx/models/StudioModel.java
@@ -1,6 +1,7 @@
 package com.example.academiacx.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
