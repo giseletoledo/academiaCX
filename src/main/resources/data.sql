3b47ea6aff023859d95b9299b1139c02c9087668
gisele <gisesonia@gmail.com>
2024-04-01 02:09:54 -0300

    feat(security): configurando sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO tb_user(name, email) VALUES ('Maria', 'maria@gmail.com');
+INSERT INTO tb_user(name, username, email, password) VALUES ('Maria', 'mariauser', 'maria@gmail.com',123456);
 INSERT INTO tb_user(name, email) VALUES ('Bob', 'bob@gmail.com');
 INSERT INTO tb_user(name, email) VALUES ('Alex', 'alex@gmail.com');
 INSERT INTO tb_user(name, email) VALUES ('Ana', 'ana@gmail.com');
