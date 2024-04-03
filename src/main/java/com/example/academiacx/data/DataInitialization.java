36fd33c4b808671076f3ee4537b0fa4341cc044a
gisele <gisesonia@gmail.com>
2024-04-03 17:47:24 -0300

    fix(sql): ajustando para visualizar cadastro

--- src/main/java/com/example/academiacx/data/DataInitialization.java
+++ src/main/java/com/example/academiacx/data/DataInitialization.java
@@ -1,50 +1,118 @@
 package com.example.academiacx.data;
 
-import com.example.academiacx.models.*;
-import com.example.academiacx.services.inter.*;
+import com.example.academiacx.models.UserModel;
+import com.example.academiacx.repository.RoleRepository;
+import com.example.academiacx.repository.UserRepository;
+import com.example.academiacx.services.*;
+
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Component
 public class DataInitialization implements CommandLineRunner {
 
     @Autowired
-    private MovieService movieService;
+    private MovieServiceImpl movieService;
 
     @Autowired
-    private ActorService actorService;
+    private ActorServiceImpl actorService;
 
     @Autowired
-    private DirectorService directorService;
+    private DirectorServiceImpl directorService;
 
     @Autowired
-    private GenreService genreService;
+    private GenreServiceImpl genreService;
 
     @Autowired
-    private StudioService studioService;
+    private StudioServiceImpl studioService;
 
     @Autowired
-    private FranchiseService franchiseService;
+    private UserRepository userRepository;
 
     @Autowired
-    private StreamingService streamingService;
+    private FranchiseServiceImpl franchiseService;
+
+    @Autowired
+    private StreamingServiceImpl streamingService;
+
+    //@Autowired
+    //private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     @Override
     @Transactional
     public void run(String... args) throws Exception {
         try {
+           /* RoleModel roleAdmin = new RoleModel();
+            roleAdmin.setName("ADMIN");
+            roleAdmin = roleRepository.save(roleAdmin);
+
+            RoleModel roleUser = new RoleModel();
+            roleUser.setName("USER");
+            roleUser = roleRepository.save(roleUser);
+
+            List<RoleModel> roles = new ArrayList<>();
+            roles.add(roleAdmin);
+            roles.add(roleUser);
+
+            UserModel user = new UserModel();
+            user.setName("Estela");
+            user.setUsername("user");
+            user.setEmail("estela@gmail.com");
+            //user.setPassword(passwordEncoder.encode("123456"));
+            user.setRoles(roles);
+            userRepository.save(user);
+
+            UserModel admin = new UserModel();
+            admin.setName("Cláudia");
+            admin.setUsername("admin");
+            admin.setEmail("claudia@gmail.com");
+            //admin.setPassword(passwordEncoder.encode("123456"));
+            admin.setRoles(Collections.singletonList(roleAdmin)); // Admin só tem uma função
+            userRepository.save(admin);*/
+
+            List<UserModel> allUsers = userRepository.findAll();
+
+            System.out.println("**List of Users:**");
+
+            for (UserModel userData : allUsers) {
+                System.out.println("**ID:** " + userData.getId());
+                System.out.println("**Name:** " + userData.getName());
+                System.out.println("**Username:** " + userData.getUsername());
+                //System.out.println("**Address:** " + userData.getAddress());
+                System.out.println("**Email:** " + userData.getEmail());
+                System.out.println("**Roles:** " + userData.getRoles());
+                System.out.println("-----------------------");
+            }
+/*
+ // Recuperar os dados do usuário com ID 1
+            Optional<UserModel> optionalUser = userRepository.findById(1L);
+
+            if (optionalUser.isPresent()) {
+                UserModel savedUser = optionalUser.get();
+                System.out.println("Dados do usuário com ID 1:");
+                System.out.println("ID: " + savedUser.getId());
+                System.out.println("Nome: " + savedUser.getName());
+                System.out.println("E-mail: " + savedUser.getEmail());
+                // E assim por diante, conforme necessário para outros campos
+            } else {
+                System.out.println("Usuário com ID 1 não encontrado.");
+            }
+
+
             // Mock dos dados para cadastrar filmes
             StudioModel studio = new StudioModel("Marvel Studios","EUA");
             GenreModel genre = new GenreModel("Ação");
@@ -52,19 +120,6 @@ public class DataInitialization implements CommandLineRunner {
             // Salvar genero e studio, por causa dos relacionamentos de tabela
             studioService.create(studio);
             genreService.create(genre);
-
-            // Criando alguns diretores
-            DirectorModel director1 = new DirectorModel("Joe Russo");
-            directorService.create(director1);
-
-            DirectorModel director2 = new DirectorModel("Anthony Russo");
-            directorService.create(director2);
-
-            DirectorModel director3 = new DirectorModel("Jon Watts");
-            directorService.create(director3);
-
-            DirectorModel director4 = new DirectorModel("John Fraveau");
-            directorService.create(director4);
 
             // Adicionando atores
             List<ActorModel> actors = new ArrayList<>();
@@ -119,11 +174,11 @@ public class DataInitialization implements CommandLineRunner {
             movieService.create(movieAvengers);
             movieService.create(movieSpiderman);
 
-            Long userId = 1L;
-            Long movieId = 1L;
+    Long userId = 1L;
+            List<Long> movieIds = List.of(1L, 2L);
 
             // Construir o objeto JSON com o ID do filme
-            String requestBody = "{\"movieIds\":" + movieId + "}";
+            String requestBody = "{\"movieIds\":" + movieIds.toString() + "}";
 
             // Criar um cliente HTTP
             HttpClient client = HttpClient.newHttpClient();
@@ -156,15 +211,11 @@ public class DataInitialization implements CommandLineRunner {
                 System.out.println("Franchise: " + movie.getFranchise().getName());
                 System.out.println("Streaming: " + movie.getStreaming().getName());
                 System.out.println("-----------------------------");
-            });
-
-
+            });*/
         } catch (Exception e) {
             // Lidar com exceções gerais
             System.err.println("Erro ao inicializar dados: " + e.getMessage());
             e.printStackTrace();
         }
-
-
     }
 }
