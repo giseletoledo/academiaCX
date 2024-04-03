1a0b2a1e2f378fbc7dd0ec9ff89570b5888a2014
gisele <gisesonia@gmail.com>
2024-04-03 17:46:34 -0300

    feat(sql): adicionando campos

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,28 @@
-INSERT INTO tb_user(name, email) VALUES ('Maria', 'maria@gmail.com');
-INSERT INTO tb_user(name, email) VALUES ('Bob', 'bob@gmail.com');
-INSERT INTO tb_user(name, email) VALUES ('Alex', 'alex@gmail.com');
-INSERT INTO tb_user(name, email) VALUES ('Ana', 'ana@gmail.com');
-INSERT INTO tb_user(name, email) VALUES ('Joao', 'joao@gmail.com');
-INSERT INTO tb_user(name, email) VALUES ('Marcello', 'marcello@gmail.com');
+-- Inserindo Maria com todos os dados
+INSERT INTO tb_user(name, username, email, password, cep, logradouro, numero, bairro, localidade, uf)
+VALUES ('Maria', 'mariauser', 'maria@gmail.com', '123456', '01310930', 'Avenida Paulista', '1200', 'Bela vista', 'São Paulo', 'SP');
+
+-- Inserindo Bob apenas com nome, email e outros campos nulos
+INSERT INTO tb_user(name, email, cep, logradouro, numero, bairro, localidade, uf)
+VALUES ('Bob', 'bob@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- Inserindo Alex apenas com nome, email e outros campos nulos
+INSERT INTO tb_user(name, email, cep, logradouro, numero, bairro, localidade, uf)
+VALUES ('Alex', 'alex@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- Inserindo Ana apenas com nome, email e outros campos nulos
+INSERT INTO tb_user(name, email, cep, logradouro, numero, bairro, localidade, uf)
+VALUES ('Ana', 'ana@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- Inserindo Joao apenas com nome, email e outros campos nulos
+INSERT INTO tb_user(name, email, cep, logradouro, numero, bairro, localidade, uf)
+VALUES ('Joao', 'joao@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- Inserindo Marcello apenas com nome, email e outros campos nulos
+INSERT INTO tb_user(name, email, cep, logradouro, numero, bairro, localidade, uf)
+VALUES ('Marcello', 'marcello@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL);
+
+
+
+
 
