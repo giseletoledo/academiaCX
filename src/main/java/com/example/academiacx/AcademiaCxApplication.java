e8624b41cfd2ccf2125ff2472b2055303237eb16
gisele <gisesonia@gmail.com>
2024-04-05 03:22:20 -0300

    fix(viacep): configuracao

--- src/main/java/com/example/academiacx/AcademiaCxApplication.java
+++ src/main/java/com/example/academiacx/AcademiaCxApplication.java
@@ -2,10 +2,10 @@ package com.example.academiacx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
+@EnableFeignClients
 public class AcademiaCxApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AcademiaCxApplication.class, args);
     }
