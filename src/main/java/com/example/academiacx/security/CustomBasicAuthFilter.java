3257bbc001735e0a985968ce2735c5c6b27276b0
gisele <gisesonia@gmail.com>
2024-04-05 08:29:30 -0300

    fix(security): configurações do security

--- src/main/java/com/example/academiacx/security/CustomBasicAuthFilter.java
+++ src/main/java/com/example/academiacx/security/CustomBasicAuthFilter.java
@@ -4,24 +4,48 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Enumeration;
+import java.util.Base64;
 
-@Component
+@Service
 public class CustomBasicAuthFilter extends OncePerRequestFilter {
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    private static final int BASIC_LENGTH = 6;
+    private static final String EXEMPLO_USERNAME = "usuario";
+    private static final String EXEMPLO_PASSWORD = "123456";
 
-        StringBuilder headersString = new StringBuilder();
-        Enumeration<String> headerNames = request.getHeaderNames();
-        while (headerNames.hasMoreElements()) {
-            String headerName = headerNames.nextElement();
-            String headerValue = request.getHeader(headerName);
-            headersString.append(String.format("%s: %s\n", headerName, headerValue));
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        // Basic dXN1YXJpbzoxMjM0NTY=
+        var headerAuthorization = request.getHeader("Authorization");
+
+        if (headerAuthorization == null || !headerAuthorization.startsWith("Basic ")) {
+            filterChain.doFilter(request, response);
+            return;
         }
+        // dXN1YXJpbzoxMjM0NTY=
+        var basicToken = headerAuthorization.substring(BASIC_LENGTH);
+        byte[] basicTokenDecoded = Base64.getDecoder().decode(basicToken);
+
+        // usuario:123456
+        String basicTokenValue = new String(basicTokenDecoded);
+
+        // ["usuario", "123456"]
+        String[] basicAuthsSplit = basicTokenValue.split(":");
+
+        if (basicAuthsSplit[0].equals(EXEMPLO_USERNAME)
+                && basicAuthsSplit[1].equals(EXEMPLO_PASSWORD)) {
+
+            var authToken = new UsernamePasswordAuthenticationToken(basicAuthsSplit[0], null, null);
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+        }
+        filterChain.doFilter(request, response);
     }
 }
--- src/main/java/com/example/academiacx/security/SecurityConfig.java
+++ src/main/java/com/example/academiacx/security/SecurityConfig.java
@@ -3,53 +3,49 @@ package com.example.academiacx.security;
 import com.example.academiacx.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private CustomBasicAuthFilter customBasicAuthFilter;
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new CustomUserDetailsService(userRepository);
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http, CustomBasicAuthFilter customBasicAuthFilter) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(
                         authorizeConfig -> {
-                            authorizeConfig.requestMatchers("/public").permitAll();
-                            //authorizeConfig.requestMatchers("/users").permitAll();
+                            authorizeConfig.requestMatchers(toH2Console()).permitAll();
+                            //authorizeConfig.requestMatchers(HttpMethod.POST);
+                            authorizeConfig.requestMatchers("/users").permitAll();
                             authorizeConfig.requestMatchers("/logout").permitAll();
-                            authorizeConfig.requestMatchers(request -> {
-                                String path = request.getServletPath();
-                                return path.equals("/public") || path.startsWith("/users");
-                            }).permitAll();
                             authorizeConfig.anyRequest().authenticated();
                         })
-                .httpBasic(Customizer.withDefaults()) // Enable basic authentication
-                .addFilterBefore(customBasicAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(customBasicAuthFilter, BasicAuthenticationFilter.class)
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(httpSecurityHeadersConfigurer -> {
+                    httpSecurityHeadersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable);
+                })
                 .build();
     }
 
